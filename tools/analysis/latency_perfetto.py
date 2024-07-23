1258a06742481395b4757430efa20ab8e07a6230
Zixian Cai <u5937495@anu.edu.au>
2024-07-23 08:30:12 +0000

    Visualize smoothed throughput and metered latency

--- tools/analysis/latency_perfetto.py
+++ tools/analysis/latency_perfetto.py
@@ -2,18 +2,18 @@
 """
 Visualizing the DaCapo latency CSV dumps using Perfetto UI
 
-Usage: ./tools/analysis/latency_perfetto.py /path/scratch/dacapo-latency-usec-simple-<n>.csv
+Usage: ./tools/analysis/latency_perfetto.py /path/scratch/dacapo-latency-usec-simple-<n>.csv [/path/scratch/dacapo-latency-usec-metered-100ms-smoothing-<n>.csv /path/scratch/dacapo-latency-usec-metered-full-smoothing-<n>.csv]
 
 The output .json.gz file can be visualized on https://ui.perfetto.dev/
 Each request is shown as a slice.
 The number of finished request for each thread over time is shown as a counter.
 
 positional arguments:
-  input            Path to simple latency CSV
+  input            Path to one or more latency CSVs
 
 optional arguments:
   -h, --help       show this help message and exit
-  --output OUTPUT  Output path (default: "dacapo-latency.json.gz")
+  --output OUTPUT  Output path
 """
 import json
 import gzip
@@ -26,40 +26,72 @@ def build_parser():
     parser.add_argument(
         "--output", help="Output path", default="dacapo-latency.json.gz", type=str
     )
-    parser.add_argument("input", help="Path to simple latency CSV")
+    parser.add_argument("input", nargs="+", help="Path to one or more latency CSVs")
     return parser
 
 
 def main():
     parser = build_parser()
     args = parser.parse_args()
-    latency_csv_path = args.input
+    latency_csv_paths = args.input
     events = []
-    with open(latency_csv_path) as fd:
-        lines = fd.readlines()
-        finished_requests = defaultdict(int)
-        for line in lines:
-            parts = line.split(",")
-            start = int(parts[0])
-            end = int(parts[1])
-            tid = int(parts[2])
+    for p in latency_csv_paths:
+        with open(p) as fd:
+            if "simple" in p:
+                event_suffix = ""
+            elif "metered-100ms-smoothing" in p:
+                event_suffix = " 100ms metered"
+            elif "metered-full-smoothing" in p:
+                event_suffix = " fully metered"
+            else:
+                raise ValueError("Unsupported latency dump file")
+            lines = fd.readlines()
+            # Record end timestamps to be sorted later
+            # For metered latency, events starting earlier might finish later
+            end_timestamps_per_thread = defaultdict(list)
+            for line in lines:
+                parts = line.split(",")
+                start = int(parts[0])
+                end = int(parts[1])
+                tid = int(parts[2])
 
-            if start == 0 and end == 0:
-                continue
-            # Request as slices
-            events.append({"name": "Req", "ph": "B", "ts": start, "pid": 0, "tid": tid})
-            events.append({"name": "Req", "ph": "E", "ts": end, "pid": 0, "tid": tid})
-            # The number of finished requests
-            finished_requests[tid] += 1
-            events.append(
-                {
-                    "name": "t{}".format(tid),
-                    "ph": "C",
-                    "ts": end,
-                    "pid": 0,
-                    "args": {"requests": finished_requests[tid]},
-                }
-            )
+                end_timestamps_per_thread[tid].append(end)
+
+                if start == 0 and end == 0:
+                    continue
+                # Request as slices
+                # Skip metered latencies as slices would overlap
+                if "simple" in p:
+                    events.append(
+                        {
+                            "name": "Req{}".format(event_suffix),
+                            "ph": "X",
+                            "ts": start,
+                            "dur": end - start,
+                            "pid": 0,
+                            "tid": tid,
+                        }
+                    )
+            for tid, end_timestamps in end_timestamps_per_thread.items():
+                sorted_end_timestamps = sorted(end_timestamps)
+                for smooth in [1, 16, 256]:
+                    i = 0
+                    while i < len(sorted_end_timestamps):
+                        events.append(
+                            {
+                                "name": "t{}".format(tid, smooth),
+                                "ph": "C",
+                                "ts": sorted_end_timestamps[i],
+                                "pid": 0,
+                                "args": {
+                                    "requests{} smooth {}".format(
+                                        event_suffix, smooth
+                                    ): i
+                                },
+                            }
+                        )
+                        i += smooth
+
     events.append(
         {"name": "Process Start", "ph": "i", "ts": 0, "pid": 0, "tid": 0, "s": "p"}
     )
